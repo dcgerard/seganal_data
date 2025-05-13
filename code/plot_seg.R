ddcd3062bf9dfb16f672ac1471588b600d0a6eed
dcgerard <gerard.1787@gmail.com>
2025-05-13 17:00:05 -0400

    start polymapr comparison

--- code/plot_seg.R
+++ code/plot_seg.R
@@ -67,9 +67,6 @@ cor(df_p[, c("mappoly", "polymapr", "segtest")], use = "pairwise.complete.obs")
 
 ## mappoly and segtest differ ----
 df_p |>
-  filter(mappoly < 0.01, segtest > 0.5)
-
-df_p |>
   filter(snp %in% c("S8_2301244", "S8_2301256", "S8_16038595", "S8_4395910")) ->
   df_m_low_s_high
 
@@ -106,6 +103,7 @@ dat_df |>
   lim_df
 
 colvec <- rev(palette.colors(n = 7))
+names(colvec) <- 0:6
 ggplot() +
   geom_point(data = dat_df, mapping = aes(x = alt, y = ref, color = geno)) +
   xlab("Alternative Count") +
@@ -147,14 +145,76 @@ bind_rows(tab_m_low_s_high, tab_s_low_m_high) |>
   xtable(display = rep("G", 6), label = "tab:snps.mp.seg") |>
   print(include.rownames = FALSE, file = "./output/figs/snps_mp_seg.txt")
 
-
 table(filter_snp(uout_sub_gl, snp == "S8_2212186")$inddf$geno)
 
 ## polymapR and segtest differ ----
 df_p |>
-  filter(snp %in% c("S8_18370562", "S8_4866982", "S8_2212186", "S8_17348718")) ->
+  filter(polymapr > 0.1, segtest < 0.01) |>
+  sample_n(1)
+
+df_p |>
+  filter(snp %in% c("S8_18370562", "S8_4866982", "S8_14976727", "S8_17348718")) ->
   df_s_low_p_high
 
 df_p |>
   filter(snp %in% c("S8_15463274", "S8_4572598", "S8_41319", "S8_10014838")) ->
   df_p_low_s_high
+
+uout_ps <- filter_snp(uout_sub_gl, snp %in% df_p_low_s_high$snp | snp %in% df_s_low_p_high$snp)
+
+snplist <- c(df_p_low_s_high$snp, df_s_low_p_high$snp)
+
+uout_ps$snpdf |>
+  select(snp, bias, seq) |>
+  expand_grid(g = 0:6) |>
+  mutate(xi = updog:::xi_fun(p = g / 6, eps = seq, h = bias),
+         slope = xi / (1 - xi),
+         intercept = 0,
+         snp = factor(snp, levels = snplist)) ->
+  slope_df
+
+
+uout_ps$inddf |>
+  mutate(
+    alt = size - ref,
+    geno = factor(geno, levels = 0:6),
+    snp = factor(snp, levels = snplist)) ->
+  dat_df
+
+dat_df |>
+  group_by(snp) |>
+  summarize(max_ref = max(ref), max_alt = max(alt)) |>
+  mutate(ref = pmax(max_ref, max_alt),
+         alt = ref) ->
+  lim_df
+
+colvec <- rev(palette.colors(n = 7))
+names(colvec) <- 0:6
+ggplot() +
+  geom_point(data = dat_df, mapping = aes(x = alt, y = ref, color = geno)) +
+  xlab("Alternative Count") +
+  ylab("Reference Count") +
+  facet_wrap(.~ snp, scales = "free", ncol = 2) +
+  theme_bw() +
+  theme(strip.background = element_rect(fill = "white")) +
+  scale_color_manual(values = colvec, name = "Posterior Mode\nGenotype") +
+  geom_abline(data = slope_df, mapping = aes(slope = slope, intercept = intercept, group = g), linetype = "dashed", color = "grey") +
+  geom_point(data = lim_df, aes(x = alt, y = ref), alpha = 0) ->
+  pl
+
+ggsave(filename = "./output/figs/snps_polymapr_seg.pdf", plot = pl, height = 7, width = 5, family = "Times")
+
+## rerun segtest approach without low counts
+uout_s_low_p_high <- filter_snp(uout_sub_gl, snp %in% df_s_low_p_high$snp)
+uout_s_low_p_high$inddf |>
+  filter(ref > 10) ->
+  uout_s_low_p_high$inddf
+sprep_s_low_p_high <- multidog_to_g(mout = uout_s_low_p_high, type = "off_gl", ploidy = 6)
+sout_s_low_p_high <- seg_multi(g = sprep_s_low_p_high$g, p1_ploidy = 6, p2_ploidy = 6, p1 = sprep_s_low_p_high$p1, p2 = sprep_s_low_p_high$p2, model = "auto", outlier = FALSE)
+
+df_s_low_p_high |>
+  select(snp, polymapr, segtest) |>
+  left_join(select(sout_s_low_p_high, snp, segtest_new = p_value)) ->
+  tab_s_low_p_high
+
+tab_s_low_p_high
